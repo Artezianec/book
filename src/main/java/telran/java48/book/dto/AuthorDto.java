61a2d649710271a94d78698ba968bca407a72c82
Artezianec <artezianplanet@yandex.ru>
2023-10-30 19:30:19 +0200

    Add func

--- src/main/java/telran/java48/book/dto/AuthorDto.java
+++ src/main/java/telran/java48/book/dto/AuthorDto.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class AuthorDto {
 	String name;
     LocalDate birthDate;
--- src/main/java/telran/java48/book/dto/BookDto.java
+++ src/main/java/telran/java48/book/dto/BookDto.java
@@ -5,10 +5,12 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class BookDto {
 	String isbn;
 	String title;
